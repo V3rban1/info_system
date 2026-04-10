f21d540023902bb0dae6689b0f7f0da42a103b60
Hiss <a8unyx916y4r@inbox.ru>
2026-04-10 12:05:34 +0300

    Create upd_pz4.py

---
+++ upd_pz4.py
@@ -0,0 +1,16 @@
+def program_module(input_data):
+    if not input_data:
+        print('Пустой ввод.')
+    elif input_data.startswith('https://'):
+        print('Ваш запрос похож на веб-ресурс.')
+    elif input_data.endswith('.txt'):
+        print('Ваш запрос похож на файл формата TXT.')
+    elif input_data.isnumeric():
+        print('Вы ввели число:', input_data)
+    else:
+        print('Ваш запрос содержит только строку.')
+
+
+if __name__ == '__main__':
+    user_input = input('Введите ваш запрос: ')
+    program_module(user_input)
